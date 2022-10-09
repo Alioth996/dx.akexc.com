cd84aa21ccfeacfd3f95f9e98adf7b36ce81aed7
alex <2844520415@qq.com>
2022-10-09 15:23:49 +0800

    chore: add go router

--- server/man.go
+++ server/man.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "com.akexc.dx/router"
 
 func main() {
-    fmt.Println("Hello, World!")
+	router.InitRouter()
 }
---
+++ server/router/app.go
@@ -0,0 +1,7 @@
+package router
+
+import "fmt"
+
+func InitRouter() {
+	fmt.Println("init router")
+}
