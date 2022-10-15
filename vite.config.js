a03a577a580b79513c99a8d9a07ad7e9df86e6a7
alex <2844520415@qq.com>
2022-10-15 17:39:53 +0800

    chore: 使用$ref 解构 dom 元素

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue({
+      reactivityTransform: true
+    }),
+    Unocss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
