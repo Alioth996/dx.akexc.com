ec8432ea83eabf807f8cf15d317a45c03ab028b2
alex <2844520415@qq.com>
2023-03-09 12:29:47 +0800

    feat: 新增竖屏提示用户横屏

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,11 @@
     <!-- 底部项目信息 -->
     <DxFooter />
 
+    <!-- 提示用户横屏设备 -->
+    <m-dialog :visible="visible">
+      <span>手机/设备横向显示效果更佳哦~</span>
+    </m-dialog>
+
   </main>
 </template>
 
@@ -20,4 +25,29 @@ import DxHeader from './components/dx-header.vue';
 import DxFooter from './components/dx-footer.vue';
 import DxPlay from './components/dx-play.vue';
 import DxNotice from './components/dx-notice.vue';
+
+import { useDialog } from "shuimo-ui";
+
+const { visible, showDialog } = useDialog();
+
+
+
+/**
+ * orientation 是 Screen 接口的一个只读属性，返回屏幕当前的方向。
+ * @api https://developer.mozilla.org/zh-CN/docs/Web/API/Screen/orientation
+ */
+let orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+
+if (orientation === "landscape-primary") {
+  console.log(`${orientation}:大屏横屏`);
+} else if (orientation === "landscape-secondary") {
+  console.log(`${orientation}:中屏横屏`);
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  console.log(`${orientation}:正常竖屏`);
+  showDialog()
+} else if (orientation === undefined) {
+  console.log("设备不支持该api");
+}
+
+
 </script>
